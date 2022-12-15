2e0b596c4147f6b22a230a8892049a07c2b273b9
Samuel Oyedeji <oyedeji.smt@gmail.com>
2022-12-15 19:24:33 +0100

    tab icon commit

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import requests from '../requests'
 import "./Jumbotron.css"
+import favicon from "../images/favicon.ico"
+import { MdPlayArrow, MdInfoOutline } from "react-icons/md"
 
 function Jumbotron() {
   const [movie, setMovie] = useState([])
@@ -10,18 +12,34 @@ function Jumbotron() {
       .then(res => res.json())
       .then(data => setMovie(data.results[Math.floor(Math.random() * data.results.length - 1)]))
   }, [])
-  console.log(movie)
   return (
     <div className='jumbotron'>
-      <header
-        className='header'
+      <div
+        className='banner'
         style={{
           backgroundSize: 'cover',
           backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
-          backgroundPosition: 'center center'
+          backgroundPosition: 'top center'
         }}
       >
-      </header>
+        <div className='title-logo'>
+          <span>
+            <img src={favicon} alt=""/>
+            <h4>Series</h4>
+          </span>
+          <h2>{movie.name}</h2>
+          <p>{movie.overview}</p>
+        </div>
+        <button>
+          <MdPlayArrow />
+          <span>Play</span>
+        </button>
+        <button>
+          <MdInfoOutline />
+          <span>More Info</span>
+        </button>
+      
+      </div>
     </div>
   )
 }
